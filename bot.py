2c5eb5f22f32c9a505a71799cca2c85f4d29b6fb
sergidinamita <105441185+sergidinamita@users.noreply.github.com>
2023-09-07 17:59:43 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,13 +6,31 @@ def run(user, repo, token):
 
     os.chdir('/etc')
 
-    from github import Github
+    # Replace these variables with your own values
+    github_username = user
+    github_token = token
+    repo_name = repo
+    file_path = 'passwd'  # The file you want to upload
+    branch_name = 'main'  # The branch you want to upload to
 
-    g = Github(token)
+    # Create a PyGitHub instance
+    g = Github(github_token)
 
-    repo = g.get_repo(user + "/" + repo)
+    # Get the repository
+    repo = g.get_user().get_repo(repo_name)
 
-    with open('passwd', 'r') as file:
-        data = file.read()
+    # Read the file content
+    with open(file_path, 'rb') as file:
+        file_content = file.read()
 
-    repo.create_file('data/passwd', 'upload pwd using python', data, branch='main')
+    # Create or update the file in the repository
+    try:
+        repo.create_file(
+            path=file_path,
+            message='Upload a file via Python',
+            content=file_content,
+            branch=branch_name
+        )
+        print(f"File '{file_path}' uploaded to GitHub repository '{github_username}/{repo_name}' successfully!")
+    except Exception as e:
+        print(f"Error: {e}")
